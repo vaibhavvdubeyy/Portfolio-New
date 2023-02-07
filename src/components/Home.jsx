7173a8db09ab7dc4360efe9ab50beca152b2366f
Vaibhav Dubey <121270051+vaibhavvdubeyy@users.noreply.github.com>
2023-02-07 17:42:17 +0530

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import '../styles/home.scss'
 import { SocialIcon } from 'react-social-icons';
+import profile from '../assets/PSP.png';
 
 const Home = () => {
   return (
     <>
     <div className='heading'>
+   
     <h1>I'm Vaibhav </h1>
     <h2>Web Developer</h2>
     </div>
+    <div className='photo'>
+    <img src={profile} id='photo' alt="profile" />
+    </div>
+    
     <div className='btn'>
         <button>RESUME</button>
     </div>
+    
     <div className='social '>
      <SocialIcon url="https://www.linkedin.com/in/vaibhav-dubey-3814a7184/" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
      <SocialIcon url="https://www.youtube.com/channel/UCltUAcF3lrhGnqjqe81QE_w" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
